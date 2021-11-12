fe5128ef84bae3b242791da0b970875ed14b532c
Chris McMillan <dcmcmillan@gmail.com>
2021-11-12 13:46:37 -0800

    Updated functions.py

---
+++ section3/functions.py
@@ -0,0 +1,11 @@
+def remainder(x, y):
+    '''
+    DOCSTRING: this returns the remainder of x divided by y
+    INPUT: Two integers x and y
+    OUTPUT: One integer remainder
+    '''
+    
+    return x % y
+
+result = remainder(10, 3)
+print(result)
